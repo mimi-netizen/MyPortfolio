c7784f72a052c37b98d7c8854885fe72181a7a1f
Celyne Kydd <dianakydd2@gmail.com>
2025-04-19 11:39:50 +0300

    configured backend and server

--- server/server.js
+++ server/server.js
@@ -19,6 +19,14 @@ app.use(express.json());
 // Serve static files from Portfolio directory
 app.use(express.static(path.join(__dirname, '../Portfolio')));
 
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.error('MongoDB Connection Error:', err));
+
 // Serve admin files
 app.use('/admin', express.static(path.join(__dirname, '../Portfolio/admin')));
 
