1efa1005ea800bf48b6414f7093c636349cfd192
Celyne Kydd <dianakydd2@gmail.com>
2025-04-20 14:36:14 +0300

    deployment

--- server.js
+++ server.js
@@ -2,25 +2,25 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files
-app.use(express.static(path.join(__dirname)));
+// Serve static files from the Portfolio directory
+app.use(express.static(path.join(__dirname, 'Portfolio')));
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'Portfolio', 'index.html'));
 });
 
 app.get('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname, 'projects.html'));
+    res.sendFile(path.join(__dirname, 'Portfolio', 'projects.html'));
 });
 
 app.get('/project-details', (req, res) => {
-    res.sendFile(path.join(__dirname, 'project-details.html'));
+    res.sendFile(path.join(__dirname, 'Portfolio', 'project-details.html'));
 });
 
 // Fallback route
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'Portfolio', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
